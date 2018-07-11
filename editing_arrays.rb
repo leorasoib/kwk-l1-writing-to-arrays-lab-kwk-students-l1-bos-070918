56cd897d73f3b9a4b448fad9fee7ef4bc4cc85dd
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-11 23:52:24 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,21 +1,17 @@
-rainbow_colors = [
+$rainbow_colors = [
   "yellow",
   "default",
   "light_cyan"
 ]
 
 def change_rainbow_colors
-  # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
-  
-  ["red","light_red","light_yellow"]
+  $rainbow_colors[0..2]="red","light_red","light_yellow"
 end
 
-rainbow_colors = change_rainbow_colors
-def add_colors(rainbow_colors)
+puts change_rainbow_colors
+def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
- rainbow_colors << ["green","blue"]
- rainbow_colors
+$rainbow_colors.push("green","blue")
 end
 
-rainbow_colors = add_colors(rainbow_colors)
-rainbow_colors
+puts $rainbow_colors
